dba3233e67a3173f778ee674062c3cbae4e91734
Joaquin Stockle <joaquinstockle@gmail.com>
2019-09-04 10:26:40 -0600

    Added TriggerEventBehaviour Script

---
+++ New Unity Project 2/Assets/Scripts/TriggerEventBehaviour.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+    public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStayEvent.Invoke();
+    }
+}
