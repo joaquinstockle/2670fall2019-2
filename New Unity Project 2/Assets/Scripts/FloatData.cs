585638c5bfd76b273e2676e578a3501eddb1d2ff
Joaquin Stockle <joaquinstockle@gmail.com>
2019-09-04 14:29:22 -0600

    Added Image Script and edited objects

--- New Unity Project 2/Assets/Scripts/FloatData.cs
+++ New Unity Project 2/Assets/Scripts/FloatData.cs
@@ -6,9 +6,35 @@ using UnityEngine;
 public class FloatData : ScriptableObject
 {
       public float value = 1f;
+      public float maxValue = 1f;
 
       public void UpdateValue(float amount)
       {
             value += amount;
       }
+      
+      public void UpdateValueLimitZero(float amount)
+      {
+            if(value < 0)
+            {
+                  value = 0;
+            }
+            else
+            {
+                  UpdateValue(amount);
+            }
+      }
+            public void UpdateValueLimitZeroAndMaxValue(float amount) 
+            {
+                 
+                  if (value < maxValue)
+                  {
+                        UpdateValue(amount);
+                  }
+                  else
+                  {
+                        value = maxValue;
+                  }
+                   UpdateValueLimitZero(amount);
+            }
 }
---
+++ New Unity Project 2/Assets/Scripts/ImageController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Image))]
+public class ImageController : MonoBehaviour
+{
+    public Image imageComponent;
+    public UnityEvent UpdateImageEvent;
+   
+    private void Start()
+    {
+        imageComponent = GetComponent<Image>();
+    }
+
+    public void UpdateImageComponent(FloatData dataObj)
+    {
+        imageComponent.fillAmount = dataObj.value;
+    }
+    
+    private void Update()
+    {
+        UpdateImageEvent.Invoke();
+    }
+}
