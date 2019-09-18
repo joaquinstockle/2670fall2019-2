d14d71f27c2c8f6febec495d43472cc07e486919
Joaquin Stockle <joaquinstockle@gmail.com>
2019-09-18 14:22:51 -0600

    ActionCalls Script added

---
+++ New Unity Project 2/Assets/Scripts/ActionCalls.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+using Vector3 = UnityEngine.Vector3;
+
+public class ActionCalls : MonoBehaviour
+{
+    public float speed = 1f, rotateSpeed = 10f, scaleSpeed = .2f;
+    private Vector3 location, rotations, scales;
+    private UnityAction transformAction;
+    private void Start()
+    {
+        transformAction = OnMove;
+    }
+    private void OnMouseDown()
+    {
+        if (transformAction == OnMove)
+        {
+            transformAction = OnRotate;
+        }
+        else if (transformAction == OnRotate)
+        {
+            transformAction = OnScale;
+        }
+        else if (transformAction == OnScale)
+        {
+            transformAction = OnMove;
+        }
+    }
+    void Update()
+    {
+        transformAction();
+    }
+    public void OnMove()
+    {
+        location.x = speed * Time.deltaTime;
+        transform.Translate(location);
+    }
+    public void OnRotate()
+    {
+        rotations.y = rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotations);
+    }
+    public void OnScale()
+    {
+        scales.Set( newX:scaleSpeed, newY: scaleSpeed, newZ: scaleSpeed);
+        transform.localScale += scales;
+    }
+}
