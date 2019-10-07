c62f49826be50fcddfd33d82d6fcebd636eccf69
Joaquin Stockle <joaquinstockle@gmail.com>
2019-10-07 14:46:00 -0600

    Added collection script

---
+++ New Unity Project 2/Assets/Scripts/Collections.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Collections : ScriptableObject
+{
+    public List<Object> objectList;
+
+    public void FindObjectType(Object obj)
+    {
+        foreach (var currentOnj in objectList)
+        {
+            if (currentOnj == obj)
+            {
+                //stuff;
+            }
+        }
+    }
+
+    public void AddToList(Object obj)
+    {
+        objectList.Add(obj);
+    }
+
+    public void RemoveFromList(Object obj)
+    {
+        foreach (var currentOnj in objectList)
+        {
+            if (currentOnj == obj)
+            {
+                objectList.Remove(obj);
+            }
+        }
+    }
+
+}
