62f40b80b8364f905d6892787b9280f57c5e2985
Joaquin Stockle <joaquinstockle@gmail.com>
2019-10-09 14:17:38 -0600

    Added Scripts but didn't finish them

---
+++ New Unity Project 2/Assets/Scripts/Classes/EndGameClass.cs
@@ -0,0 +1,12 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameClass : MonoBehaviour
+{
+    public GameAction gameActionObject;
+    
+    private void 
+}
+*/
