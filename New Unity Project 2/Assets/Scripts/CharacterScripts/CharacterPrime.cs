d3f431947d3f114c984557be5d1a02eb2709c481
Joaquin Stockle <joaquinstockle@live.com>
2019-10-30 14:29:37 -0600

    Made character controller script

---
+++ New Unity Project 2/Assets/Scripts/CharacterScripts/CharacterPrime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+
+public abstract class CharacterPrime : CharacterBase
+{
+    public override void Move()
+    {
+
+        Location.Set(Input.GetAxis("Vertical") * speed, 0, 0);
+        Orientation.y = Input.GetAxis("Horizontal")*orientSpeed;
+
+        Controller.transform.Rotate(Orientation);
+        Location = Controller.transform.TransformDirection(Location);
+        
+        Controller.Move(Location * Time.deltaTime);
+    }
+    
+}
